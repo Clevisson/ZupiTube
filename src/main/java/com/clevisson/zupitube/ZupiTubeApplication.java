0516c42deac2cd627f390202c994f0c29ace523e
clevisson <clevissonalgebra@gmail.com>
2020-08-25 21:47:28 -0300

    iniciando projeto

---
+++ src/main/java/com/clevisson/zupitube/ZupiTubeApplication.java
@@ -0,0 +1,13 @@
+package com.clevisson.zupitube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupiTubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZupiTubeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clevisson/zupitube/ZupiTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clevisson.zupitube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupiTubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
